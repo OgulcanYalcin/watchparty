6f728dff6c6d46cf21e0effe129699ab82497e6d
Ogulcan Yalcin <ogul__can__16@hotmail.com>
2026-07-19 16:33:53 +0300

    Vercel Web Analytics ekle

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,12 +4,14 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
 import { AuthProvider } from './store/AuthProvider.tsx'
+import { Analytics } from "@vercel/analytics/react";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <AuthProvider>
         <App />
+        <Analytics />
       </AuthProvider>
     </BrowserRouter>
   </StrictMode>,
